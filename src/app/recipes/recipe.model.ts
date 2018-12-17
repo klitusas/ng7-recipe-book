db3773b87df076a7c41f0b17cae04ddfbd43373f
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-17 09:54:10 +0100

    fix(model): fix recipes model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,12 @@
 export class Recipe {
     public name: string; //express that can be accessed outside
-    public descritpion: string;
+    public description: string;
     public imagePath: string;
 
     //add constructor so that i can instansiate with a 'new' keyword
     constructor(name: string, desc: string, imagePath: string){
         this.name = name;
-        this.descritpion = desc;
+        this.description = desc;
         this.imagePath = imagePath;
     }
 }
