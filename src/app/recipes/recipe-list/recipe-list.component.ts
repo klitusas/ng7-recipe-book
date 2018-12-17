01138f6f965ab6f2f8f280c9e6288550b3d46335
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-17 09:54:49 +0100

    feat(recipe-list): populate list with recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Recipe } from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,7 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+   //must be an array of Recipes (Recipe[])
+   recipes: Recipe[] = [ 
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg'),
+    new Recipe('A Test Recipe1', 'This is simply a test1', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
+  ]; 
+
+  constructor() { 
+    console.log(this.recipes)
+  }
 
   ngOnInit() {
   }
