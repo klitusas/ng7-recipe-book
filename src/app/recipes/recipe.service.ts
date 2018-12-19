cb5a7871e0f9123afaa773d462e843da65e373f8
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-19 15:58:01 +0100

    feat(services): create recipe and shopping list services

---
+++ src/app/recipes/recipe.service.ts
@@ -0,0 +1,16 @@
+import { Recipe } from '../recipes/recipe.model'
+import { TouchSequence } from 'selenium-webdriver';
+
+export class RecipeService {
+    //must be an array of Recipes (Recipe[])
+    //let make it private so that you cant access it from outside
+   private recipes: Recipe[] = [ 
+    new Recipe('A Test Recipe', 'Combine garlic, ginger, turmeric, garam masala, coriander, and cumin in a small bowl. Whisk yogurt, salt, and half of spice mixture in a medium bowl; add chicken and turn to coat. Cover and chill 4-6 hours. Cover and chill remaining spice mixture. Heat ghee in a large heavy pot over medium heat. Add onion, tomato paste, cardamom, and chiles and cook, stirring often, until tomato paste has darkened and onion is soft, about 5 minutes. Add remaining half of spice mixture and cook, stirring often, until bottom of pot begins to brown, about 4 minutes. Add tomatoes with juices, crushing them with your hands as you add them. Bring to a boil, reduce heat, and simmer, stirring often and scraping up browned bits from bottom of pot, until sauce thickens, 8-10 minutes. Add cream and chopped cilantro. Simmer, stirring occasionally, until sauce thickens, 30-40 minutes. Meanwhile, preheat broiler. Line a rimmed baking sheet with foil and set a wire rack inside sheet. Arrange chicken on rack in a single layer. Broil until chicken starts to blacken in spots (it will not be cooked through), about 10 minutes. Cut chicken into bite-size pieces, add to sauce, and simmer, stirring occasionally, until chicken is cooked through, 8-10 minutes. Serve with rice and cilantro sprigs.', 'https://api.norecipes.com/wp-content/uploads/2018/08/teriyaki-chicken-recipe_007.jpg'),
+    new Recipe('A Test Recipe1', 'This is simply a test1', 'https://res.cloudinary.com/hellofresh/image/upload/f_auto,fl_lossy,h_436,q_auto/v1/hellofresh_s3/image/enchiladas-aux-legumes-1a1102aa.jpg')
+  ]; 
+    
+  getRecipes(){
+      return this.recipes.slice();
+  }
+
+}
