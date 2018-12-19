f062e6026528a43750acd57f10e9ca1e36372a5b
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-19 17:09:40 +0100

    feat(recipe.model): add ingredients[] to recipe model

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,12 +1,16 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class Recipe {
     public name: string; //express that can be accessed outside
     public description: string;
     public imagePath: string;
+    public ingredients: Ingredient[]
 
     //add constructor so that i can instansiate with a 'new' keyword
-    constructor(name: string, desc: string, imagePath: string){
+    constructor(name: string, desc: string, imagePath: string, ingredients: Ingredient[]){
         this.name = name;
         this.description = desc;
         this.imagePath = imagePath;
+        this.ingredients = ingredients;
     }
 }
