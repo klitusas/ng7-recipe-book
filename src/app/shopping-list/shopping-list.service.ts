6c6d252dd55df4861a252700ff69cf7a4ec92b64
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-19 16:56:20 +0100

    feat(shopping-list-service): add shopping list service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,22 @@
+import { Ingredient } from '../shared/ingredient.model';
+import { EventEmitter } from '@angular/core';
+
 export class ShoppingListService {
-    
+
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apple', 5),
+        new Ingredient('Tomato', 10)
+    ]; 
+
+    getIngredients() {
+        return [...this.ingredients]
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        //dealing with array copies, avoiding mutating original
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 }
