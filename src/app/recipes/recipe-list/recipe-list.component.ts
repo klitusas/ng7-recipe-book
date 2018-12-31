0e539a1c2faaa4216eb3a0684facb96e35a49693
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-31 14:16:29 +0100

    fix(subscription): destroy listeners

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Recipe } from '../recipe.model'
 import { RecipeService } from '../recipe.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
   //dont need to provide the parent component has it injected
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit, OnDestroy {
 
    //must be an array of Recipes (Recipe[])
   recipes: Recipe[];
+  subscription: Subscription;
 
   constructor(private recipeService: RecipeService, private router: Router, private route: ActivatedRoute) { 
   }
 
   ngOnInit() {
-    this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
+    this.subscription = this.recipeService.recipesChanged.subscribe((recipes: Recipe[]) => {
       this.recipes = recipes;
     })
     this.recipes = this.recipeService.getRecipes();
@@ -25,6 +27,10 @@ export class RecipeListComponent implements OnInit {
 
   onNewRecipe() {
     this.router.navigate(['new'], {relativeTo: this.route})
+  }
+
+  ngOnDestroy(){
+    this.subscription.unsubscribe();
   }
 
 }
