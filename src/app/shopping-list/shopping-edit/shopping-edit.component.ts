f8648bcffd50c4dbef37b4991be40b3169ac044b
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2018-12-31 10:39:35 +0100

    feat(edit): loading shopping list items into the form for editing

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,25 +1,48 @@
-import { Component, OnInit, ViewChild, ElementRef} from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, OnDestroy } from '@angular/core';
 import { Ingredient } from 'src/app/shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list.service';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
+export class ShoppingEditComponent implements OnInit, OnDestroy {
   // @ViewChild('nameInput') nameInputRef: ElementRef;
   // @ViewChild('amountInput') amountInputRef: ElementRef;
+  @ViewChild('f') slForm: NgForm;
+  subscription: Subscription;
   ingredients: Ingredient[];
+  editMode = false;
+  editedItemIndex: number;
+  editedItem: Ingredient;
   constructor(private slService: ShoppingListService) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.subscription = this.slService.startedEditing.subscribe((index: number) => {
+        this.editedItemIndex = index;
+        this.editMode = true;
+        this.editedItem = this.slService.getIngredient(index);
+        this.slForm.setValue({
+            name: this.editedItem.name,
+            amount: this.editedItem.amount
+        })
+    })
+  }
+  ngOnDestroy(){
+    /** 
+     * Since the Subject type of subscritpion is done by us we need to
+     * unsubscribe since angular wont clean it for us 
+    */
+    this.subscription.unsubscribe();
+  }
 
   onAddItem(form: NgForm) {
-    const value = form.value;
     // const ingredientName = this.nameInputRef.nativeElement.value;
     // const ingredientAmount = this.amountInputRef.nativeElement.value;
+    const value = form.value;
     const newIngredient = new Ingredient(value.name, value.amount);
     this.slService.addIngredient(newIngredient);
   }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -6,25 +6,35 @@ export class ShoppingListService {
     /** 
      * The better practice is to use Subject
      * ingredientsChanged = new EventEmitter<Ingredient[]>();
-    */
+      * A Subject is a special type of Observable that allows values to be
+      * multicasted to many Observables. Subjects are like EventEmitters.
+      *
+      * Every Subject is an Observable and an Observer. You can subscribe to a
+      * Subject, and you can call next to feed values as well as error and complete.
+      *
+      */
     ingredientsChanged = new Subject<Ingredient[]>();
+    startedEditing = new Subject<number>();
 
     private ingredients: Ingredient[] = [
         new Ingredient('Apple', 5),
         new Ingredient('Tomato', 10)
-    ]; 
+    ];
 
     getIngredients() {
         return [...this.ingredients]
     }
-
+    
+    getIngredient(index: number) {
+        return this.ingredients[index];
+    }
     addIngredient(ingredient: Ingredient) {
         //dealing with array copies, avoiding mutating original
         this.ingredients.push(ingredient);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 
-    addIngredients(ingredients: Ingredient[]){
+    addIngredients(ingredients: Ingredient[]) {
         this.ingredients.push(...ingredients);
         this.ingredientsChanged.next(this.ingredients.slice());
     }
