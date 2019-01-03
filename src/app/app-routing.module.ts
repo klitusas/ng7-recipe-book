c74895c155f40d88fedc105f2a380cffbdd4365a
Andrejus Solovjovas <yandrejus.solovjovas@amaris.com>
2019-01-03 09:53:29 +0100

    featsignup/signin/firebase): create signup authentication

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,8 @@ import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.com
 import { Recipe } from './recipes/recipe.model';
 import { RecipesStartComponent } from './recipes/recipes-start/recipes-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
 
 const appRoutes: Routes = [
     {path: '', redirectTo: '/recipes', pathMatch: 'full'},
@@ -15,7 +17,8 @@ const appRoutes: Routes = [
         {path: ':id', component: RecipeDetailComponent},
         {path: ':id/edit', component: RecipeEditComponent},
     ]},
-    {path: 'shopping-list', component: ShoppingListComponent}
+    {path: 'shopping-list', component: ShoppingListComponent},
+    {path: 'signup', component: SignupComponent}
   ]
 
   @NgModule({
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   loadedFeature = 'recipe';
 
   onNavigate(feature:string){
     this.loadedFeature = feature;
+  }
+
+  ngOnInit(){
+    firebase.initializeApp({
+      apiKey: "AIzaSyDsJMPBR4VDQWk5auonoTisDSsAogSNRcY",
+      authDomain: "ng-recipe-book-b1647.firebaseapp.com"
+    });
   }
   
 }
